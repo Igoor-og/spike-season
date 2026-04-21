65eb9cb3d32d6e26286d33cebbb50e7a49e369cd
Igoor-og <igoralmeidadesantana0@gmail.com>
2026-04-21 19:30:52 -0300

    Update checkout.js

--- js/checkout.js
+++ js/checkout.js
@@ -11,6 +11,7 @@ const PAYMENT_METHOD = 'PIX';
 const PIX_KEY = "4691164a-5f55-4ad4-bff3-f3040c3e93d3";
 const STORE_NAME = "1629";
 const STORE_CITY = "SAO_PAULO";
+
 const SHIPPING_VALUES = {
     'SP': 15, 'RJ': 15, 'MG': 15, 'ES': 15,
     'PR': 18, 'SC': 18, 'RS': 18,
